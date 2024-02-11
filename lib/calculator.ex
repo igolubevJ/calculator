7ab58280848a397be00ba8f7ea96db248d489872
igolubev <igolubevapp@gmail.com>
2024-02-11 05:07:40 +0300

    clearing

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,18 +1,2 @@
 defmodule Calculator do
-  @moduledoc """
-  Documentation for `Calculator`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Calculator.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
