c44ac2ae23e275a72f319208012f2f5055cd5432
igolubev <igolubevapp@gmail.com>
2024-02-11 06:49:14 +0300

    Add module doc

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,4 +1,42 @@
 defmodule Calculator do
+  @moduledoc """
+  The module contains basic mathematical operations on numbers:
+
+  - addition
+  - subtraction
+  - multiplication
+  - separation
+
+  Each function returns the results of calculations that allow we need
+  to make comp chains.
+
+  #### Examples
+
+      iex> Calculator.add(42, 4) |> Calculator.div(2)
+      23.0
+
+  If the function does not support the given arguments, the function
+  will return nil
+
+  #### Examlpes
+
+      iex> Calculator.add("42", 4)
+      nil
+
+  The module has a division function that always returns `float`
+  and when divided by 0 will return `nil`
+
+  #### Examples
+
+      iex> Calculator.div(42, 2)
+      21.0
+
+      iex> Calculator.div(42, 0)
+      nil
+
+      iex> Calculator.div(42, 0.0)
+      nil
+  """
   @spec add(number(), number()) :: number() | nil
   def add(num_a, num_b), do: calculate({:add, num_a, num_b})
 
