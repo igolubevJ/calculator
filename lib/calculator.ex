dd94a13154df6475913d07f3db0f72bd514227de
igolubev <igolubevapp@gmail.com>
2024-02-11 06:02:14 +0300

    refactoring

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,29 +1,31 @@
 defmodule Calculator do
   @spec add(number(), number()) :: number() | nil
-  def add(num_a, num_b) when is_number(num_a) and is_number((num_b)) do
+  def add(num_a, num_b), do: calculate({:add, num_a, num_b})
+
+  @spec sub(number(), number()) :: number() | nil
+  def sub(num_a, num_b), do: calculate({:sub, num_a, num_b})
+
+  @spec mul(number(), number()) :: number() | nil
+  def mul(num_a, num_b), do: calculate({:mul, num_a, num_b})
+
+  @spec div(number(), number()) :: float() | nil
+  def div(num_a, num_b), do: calculate({:div, num_a, num_b})
+
+  defp calculate({:add, num_a, num_b}) when is_number(num_a) and is_number(num_b) do
     num_a + num_b
   end
 
-  def add(_, _), do: nil
-
-  @spec sub(number(), number()) :: number() | nil
-  def sub(num_a, num_b) when is_number(num_a) and is_number(num_b) do
+  defp calculate({:sub, num_a, num_b}) when is_number(num_a) and is_number(num_b) do
     num_a - num_b
   end
 
-  def sub(_, _), do: nil
-
-  @spec mul(number(), number()) :: number() | nil
-  def mul(num_a, num_b) when is_number(num_a) and is_number(num_b) do
+  defp calculate({:mul, num_a, num_b}) when is_number(num_a) and is_number(num_b) do
     num_a * num_b
   end
 
-  def mul(_, _), do: nil
-
-  @spec div(number(), number()) :: float() | nil
-  def div(num_a, num_b) when is_number(num_a) and is_number(num_b) do
+  defp calculate({:div, num_a, num_b}) when is_number(num_a) and is_number(num_b) do
     num_a / num_b
   end
 
-  def div(_, _), do: nil
+  defp calculate(_), do: nil
 end
