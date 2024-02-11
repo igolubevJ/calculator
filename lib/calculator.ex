14b636726365cd43d3b00734c098518e024d50ce
igolubev <igolubevapp@gmail.com>
2024-02-11 07:01:40 +0300

    create add documentation

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -37,6 +37,35 @@ defmodule Calculator do
       iex> Calculator.div(42, 0.0)
       nil
   """
+
+  @doc """
+  The function returns the sum of two numbers
+
+  #### Paramaters
+
+    - num_a: Number of type `int` or `float`
+    - num_b: Number of type `int` or `float`
+
+  #### Examples
+      iex> Calculator.add(42, 2)
+      44
+
+      iex> Calculator.add(42.0, 2)
+      44.0
+
+      iex> Calculator.add(42, -2)
+      40
+
+  Returns nil if invalid arguments are passed.
+
+  #### Examples
+
+      iex> Calculator.add(42, :ok)
+      nil
+
+      iex> Calculator.add(40, "A")
+      nil
+  """
   @spec add(number(), number()) :: number() | nil
   def add(num_a, num_b), do: calculate({:add, num_a, num_b})
 
@@ -63,6 +92,7 @@ defmodule Calculator do
 
   defp calculate({:div, _, 0}), do: nil
   defp calculate({:div, _, 0.0}), do: nil
+
   defp calculate({:div, num_a, num_b}) when is_number(num_a) and is_number(num_b) do
     num_a / num_b
   end
