11bba49b880c9e83a125c5450dbc2b4cc567255c
igolubev <igolubevapp@gmail.com>
2024-02-11 05:23:13 +0300

    create add feature

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,2 +1,8 @@
 defmodule Calculator do
+  @spec add(number(), number()) :: number() | nil
+  def add(num_a, num_b) when is_number(num_a) and is_number((num_b)) do
+    num_a + num_b
+  end
+
+  def add(_, _), do: nil
 end
--- test/calculator_test.exs
+++ test/calculator_test.exs
@@ -1,4 +1,27 @@
 defmodule CalculatorTest do
   use ExUnit.Case
   doctest Calculator
+
+  test "returns the result of addition two numbers" do
+    assert Calculator.add(42, 2) == 44
+    assert Calculator.add(42.5, 2.5) == 45
+    assert Calculator.add(42, 2.5) == 44.5
+    assert Calculator.add(42.5, 2) == 44.5
+  end
+
+  test "returns nil if we are passing wrong args" do
+    assert Calculator.add("42", "2") == nil
+    assert Calculator.add(42, "2") == nil
+    assert Calculator.add("42", 2) == nil
+    assert Calculator.add([42], 2) == nil
+    assert Calculator.add(42, [2]) == nil
+    assert Calculator.add({42}, 2) == nil
+    assert Calculator.add(42, {2}) == nil
+    assert Calculator.add(%{x: 42}, 2) == nil
+    assert Calculator.add(42, %{x: 2}) == nil
+    assert Calculator.add(nil, 2) == nil
+    assert Calculator.add(42, nil) == nil
+    assert Calculator.add(true, 2) == nil
+    assert Calculator.add(42, false) == nil
+  end
 end
